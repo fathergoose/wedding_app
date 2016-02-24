642692d692ff938e59a0d515577ffdf6f969de3f
fathergoose <al@ilseman.com>
2016-02-24 14:44:20 -0600

    searching comments now too

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -1,7 +1,7 @@
 class Rsvp < ActiveRecord::Base
   def self.search(search)
     if search
-      Rsvp.where('guest LIKE :search OR plusone LIKE :search  OR email LIKE :search', search: "%#{search}%")
+      Rsvp.where('guest LIKE :search OR plusone LIKE :search  OR email LIKE :search OR comment LIKE :search', search: "%#{search}%")
     else
       Rsvp.all
     end
