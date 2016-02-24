0b731261f61002e73b66ff9bf4ff62a72b6a9974
fathergoose <al@ilseman.com>
2016-02-24 11:40:52 -0600

    changed root to rsvps/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/welcome'
-
   resources :rsvps
-  root 'pages#welcome'
+  root 'rsvps#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
