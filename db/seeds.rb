c473961954055b9716c7740b1cd8432451654446
fathergoose <al@ilseman.com>
2016-04-12 19:06:47 -0500

    seed my dev user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+if Rails.env == 'development'
+  al = User.new(email: 'al@ilseman.com', password: 'password')
+  al.save()
+end
