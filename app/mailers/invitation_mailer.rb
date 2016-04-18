b6a23de5ae9ab24694af758f5201a304c0f889c7
fathergoose <al@ilseman.com>
2016-04-18 17:59:10 -0500

    constructing mailgun default emails

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,18 +1,40 @@
 class InvitationMailer < ApplicationMailer
 
+  subject: "We're getting married!"
+
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.invitation_mailer.invite.subject
   #
-  def invite_single(guest)
-    @guest = guest
+  def invite_single(guests)
+    emails = []
+    names = []
 
-    mail(to: guest.email,
-         subject: "We're getting married!" )
+    guests.each do |guest|
+      emails << guest.email
+      names << guest.first_name
+    end
+
+    mail(to: emails)
+
+    mail.mailgun_recipient_variables = Hash[emails.zip names]
+
   end
 
-  def invite_with_plusone(guest)
+  def invite_with_plusone(guests)
+    emails = []
+    names = []
+
+    guests.each do |guest|
+      emails << guest.email
+      names << guest.first_name
+    end
+
+    mail(to: emails)
+
+    mail.mailgun_recipient_variables = Hash[emails.zip names]
+
   end
 
 
